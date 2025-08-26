4ddaf79b5df745c03241ebeb5bcfd13987588451
Jupiter Bania <www.rohanbania009@gmail.com>
2025-08-26 04:04:32 +0000

    Mai yh vercel mai free mai host Krna chata hun

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCD5H0G_4X5tjT1k7G9W6GZEZw7QOE4hgA",
-  authDomain: "taskflow-74e65.firebaseapp.com",
-  projectId: "taskflow-74e65",
-  storageBucket: "taskflow-74e65.firebasestorage.app",
-  messagingSenderId: "1097057714815",
-  appId: "1:1097057714815:web:027743fc91d9e0bb03354a",
-  measurementId: "G-DPQ5PCDM7Y"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
