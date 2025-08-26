0d2388c2f240a0860f4bb69e4724bc6a0d5ba315
Jupiter Bania <www.rohanbania009@gmail.com>
2025-08-26 05:00:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,17 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCD5H0G_4X5tjT1k7G9W6GZEZw7QOE4hgA",
+  authDomain: "taskflow-74e65.firebaseapp.com",
+  projectId: "taskflow-74e65",
+  storageBucket: "taskflow-74e65.appspot.com",
+  messagingSenderId: "1097057714815",
+  appId: "1:1097057714815:web:027743fc91d9e0bb03354a",
+  measurementId: "G-DPQ5PCDM7Y"
 };
+
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
